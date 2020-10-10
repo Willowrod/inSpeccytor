53313410caae04ee944058cb5099423e9c4fefb9
Mike Hall <mike@willowrod.com>
2020-10-10 12:02:05 +0100

    Initial Commit

---
+++ inSpeccytor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  inSpeccytor
+//
+//  Created by Mike Hall on 10/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
